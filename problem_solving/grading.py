b7e66f32bc88c8b9489b7659ce4d2d05f5be3761
ibrahimfe <ibrahiimm.f@gmail.com>
2023-11-16 21:06:56 +0700

    can be optimized

---
+++ problem_solving/grading.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    rounded_grades = []
+    for a in grades:
+        if a < 38:
+            rounded_grades.append(a)
+        elif 5 - a % 5 < 3:
+            a = a + (5 - a % 5 )
+            rounded_grades.append(a)
+        else:
+            rounded_grades.append(a)
+    return rounded_grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ problem_solving/jim_and_the_orders.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jimOrders' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts 2D_INTEGER_ARRAY orders as parameter.
+#
+
+def jimOrders(orders):
+    arr = []
+    for i in range(len(orders)):
+        arr.append(sum(arr[0]))
+    return arr
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    orders = []
+
+    for _ in range(n):
+        orders.append(list(map(int, input().rstrip().split())))
+
+    result = jimOrders(orders)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
